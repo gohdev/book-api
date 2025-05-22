34b53699c60f53c9289235d912bab74bce1c866c
gohdev <76275798+gohdev@users.noreply.github.com>
2025-05-22 17:52:25 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# book-api
+
+## Python FastAPI Tutorial: How to build modern RESTful APIs with Python
+
+YouTube: https://www.youtube.com/watch?v=MCVcAAoDJS8&list=PLK8U0kF0E_D6l19LhOGWhVZ3sQ6ujJKq_&index=2
+
+### Run server
+
+```shell
+uvicorn main:app --reload
+```
+
+### Swagger UI documentation
+
+http://127.0.0.1:8000/docs
---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
+from uuid import UUID
+
+app = FastAPI()
+
+class Book(BaseModel):
+    id: UUID
+    title: str = Field(min_length=1)
+    author: str = Field(min_length=1, max_length=100)
+    description: str = Field(min_length=1, max_length=100)
+    rating: int = Field(gt=-1, lt=101)
+
+BOOKS = []
+
+@app.get("/")
+def read_books():
+    return BOOKS
+
+@app.post("/books")
+def create_book(book: Book):
+    BOOKS.append(book)
+    return book
