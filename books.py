3f5696fe6aa5509222117d8a2acbf7a6b80078f1
gohdev <76275798+gohdev@users.noreply.github.com>
2025-05-23 16:53:54 +0800

    Update & Delete book

--- books.py
+++ books.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI, HTTPException, Depends
 from pydantic import BaseModel, Field
-from uuid import UUID
 import models
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
@@ -22,8 +21,6 @@ class Book(BaseModel):
     description: str = Field(min_length=1, max_length=100)
     rating: int = Field(gt=-1, lt=101)
 
-BOOKS = []
-
 @app.get("/")
 def read_books(db: Session = Depends(get_db)):
     return db.query(models.Books).all()
@@ -43,21 +40,34 @@ def create_book(book: Book, db: Session = Depends(get_db)):
     return book
 
 @app.put("/{book_id}")
-def update_book(book_id: UUID, book: Book):
-    counter = 0
-    for x in BOOKS:
-        counter += 1
-        if x.id == book_id:
-            BOOKS[counter - 1] = book
-            return BOOKS[counter - 1]
-    raise HTTPException(status_code=404, detail=f"Book {book_id} does not exist")
+def update_book(book_id: int, book: Book, db: Session = Depends(get_db)):
+    book_model = db.query(models.Books).filter(models.Books.id == book_id).first()
+
+    if book_model is None:
+        raise HTTPException(
+            status_code=404,
+            detail=f"Book {book_id} does not exist"
+        )
+
+    book_model.title = book.title
+    book_model.author = book.author
+    book_model.description = book.description
+    book_model.rating = book.rating
+
+    db.add(book_model)
+    db.commit()
+
+    return book
 
 @app.delete("/{book_id}")
-def delete_book(book_id: UUID):
-    counter = 0
-    for x in BOOKS:
-        counter += 1
-        if x.id == book_id:
-            del BOOKS[counter - 1]
-            return f"Book {book_id} deleted"
-    raise HTTPException(status_code=404, detail=f"Book {book_id} does not exist")
+def delete_book(book_id: int, book: Book, db: Session = Depends(get_db)):
+    book_model = db.query(models.Books).filter(models.Books.id == book_id).first()
+
+    if book_model is None:
+        raise HTTPException(
+            status_code=404,
+            detail=f"Book {book_id} does not exist"
+        )
+
+    db.delete(book_model)
+    db.commit()
