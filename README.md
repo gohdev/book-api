0712afc3477755e828c46d8d983b8c2fff4d842c
gohdev <76275798+gohdev@users.noreply.github.com>
2025-05-23 16:42:23 +0800

    Connect to sqlite database

--- README.md
+++ README.md
@@ -1,15 +1,27 @@
 # book-api
 
-## Python FastAPI Tutorial: How to build modern RESTful APIs with Python
+## 1. Python FastAPI Tutorial: How to build modern RESTful APIs with Python
 
 YouTube: https://www.youtube.com/watch?v=MCVcAAoDJS8&list=PLK8U0kF0E_D6l19LhOGWhVZ3sQ6ujJKq_&index=2
 
 ### Run server
 
 ```shell
-uvicorn main:app --reload
+uvicorn books:app --reload
 ```
 
 ### Swagger UI documentation
 
 http://127.0.0.1:8000/docs
+
+## 2. Python FastAPI Tutorial: How to Connect FastAPI to Database
+
+YouTube: https://www.youtube.com/watch?v=34jQRPssM5Q&list=PLK8U0kF0E_D6l19LhOGWhVZ3sQ6ujJKq_&index=3
+
+SQL Alchemy - Object Relational Mapping (ORM) for database
+
+```shell
+ pip3 install sqlalchemy
+```
+
+Database: sqlite
--- books.py
+++ books.py
@@ -1,11 +1,22 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends
 from pydantic import BaseModel, Field
 from uuid import UUID
+import models
+from database import engine, SessionLocal
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
+models.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 class Book(BaseModel):
-    id: UUID
     title: str = Field(min_length=1)
     author: str = Field(min_length=1, max_length=100)
     description: str = Field(min_length=1, max_length=100)
@@ -14,12 +25,21 @@ class Book(BaseModel):
 BOOKS = []
 
 @app.get("/")
-def read_books():
-    return BOOKS
+def read_books(db: Session = Depends(get_db)):
+    return db.query(models.Books).all()
 
 @app.post("/books")
-def create_book(book: Book):
-    BOOKS.append(book)
+def create_book(book: Book, db: Session = Depends(get_db)):
+
+    book_model = models.Books()
+    book_model.title = book.title
+    book_model.author = book.author
+    book_model.description = book.description
+    book_model.rating = book.rating
+
+    db.add(book_model)
+    db.commit()
+
     return book
 
 @app.put("/{book_id}")
